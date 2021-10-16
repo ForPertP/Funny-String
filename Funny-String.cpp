538715ae1886d63bad424f7a249cf444cc9b26b2
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-10-17 04:07:56 +0900

    Update Funny-String.cpp

--- Funny-String.cpp
+++ Funny-String.cpp
@@ -1,1 +1,26 @@
+string funnyString(string s)
+{
+    std::string result {"Funny"};
+    std::string r = s;
+    std::reverse(r.begin(), r.end());
+
+    bool isFunny = true;
+    int len = s.length();
+    
+    for (int i = 1; i < len; i++)
+    {
+        if (abs(s[i] - s[i-1]) != abs(r[i] - r[i-1]))
+        {
+            isFunny = false;
+            break;
+        }     
+    }
+    
+    if (isFunny == false)
+    {
+        result.insert(0, "Not ");
+    }    
+    
+    return result;    
+}
 
