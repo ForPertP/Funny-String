bbad5d4bf175d9dc46b93bbb6eb40f83a2be6612
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-01 03:30:17 +0900

    Update Funny-String.cpp

--- Funny-String.cpp
+++ Funny-String.cpp
@@ -85,3 +85,22 @@ string rtrim(const string &str) {
     return s;
 }
 
+string ltrim(const string &str)
+{
+    string s(str);
+    s.erase(
+        s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
+    );
+    return s;
+}
+
+
