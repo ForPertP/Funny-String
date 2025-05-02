a86a4ee8df6159f040b60bc1cb7dc1a5aefabd5c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-03 03:13:08 +0900

    Update Funny-String.cs

--- Funny-String.cs
+++ Funny-String.cs
@@ -1,1 +1,25 @@
-Funny-String
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+}
