19aab82ed2699babb0843e492f753d3abdd9f338
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-03 03:24:10 +0900

    Update Funny-String.cs

--- Funny-String.cs
+++ Funny-String.cs
@@ -25,7 +25,7 @@ class Result
     public static string funnyString(string s)
     {
         int len = s.Length;
-        
+
         for (int i = 1; i < len; i++)
         {
             if (Math.Abs(s[i] - s[i - 1]) != Math.Abs(s[len - i] - s[len - i - 1]))
@@ -33,34 +33,56 @@ class Result
                 return "Not Funny";
             }
         }
-        
+
         return "Funny";
-    }    
+    }
+
 
     public static string funnyString2(string s)
     {
         string result = "Funny";
         string r = new string(s.Reverse().ToArray());
-    
+
         bool isFunny = true;
         int len = s.Length;
-        
+
         for (int i = 1; i < len; i++)
         {
-            if (Math.Abs(s[i] - s[i-1]) != Math.Abs(r[i] - r[i-1]))
+            if (Math.Abs(s[i] - s[i - 1]) != Math.Abs(r[i] - r[i - 1]))
             {
                 isFunny = false;
                 break;
-            }     
+            }
         }
-        
-        if (isFunny == false)
+
+        if (!isFunny)
         {
             result = "Not " + result;
-        }    
-        
-        return result;    
-    }
+        }
 
-    
+        return result;
+    }
+}
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            string result = Result.funnyString(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
 }
